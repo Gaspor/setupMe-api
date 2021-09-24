972beab59e9530fb1f2d3c055b749c634fa1f145
Gaspor <gaspor991@gmail.com>
2021-09-24 19:53:04 -0300

    add: express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("Running");
 });
 
