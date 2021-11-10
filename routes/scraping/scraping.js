dd6e8244000e832297a2f9d30b9b45c1ccfe269e
Gaspor <gaspor991@gmail.com>
2021-11-10 11:11:27 -0300

    add: scraping routes

--- routes/scraping/scraping.js
+++ routes/scraping/scraping.js
@@ -6,7 +6,7 @@ app.get('/armazenamento', async (req, res) => {
     const armazenamento = require("./../../resources/getjsons/armazenamento");
     var { json } = await armazenamento.getHD();
     
-    ws.getPriceValue(json, "armazenamento");
+    await ws.getPriceValue(json, "armazenamento");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
@@ -15,7 +15,7 @@ app.get('/placa_mae', async (req, res) => {
     const placa_mae = require("./../../resources/getjsons/placa_mae");
     var { json } = await placa_mae.getMotherboards();
     
-    ws.getPriceValue(json, "placa_mae");
+    await ws.getPriceValue(json, "placa_mae");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
@@ -24,7 +24,7 @@ app.get('/placa_video', async (req, res) => {
     const placa_video = require("./../../resources/getjsons/placa_video");
     var { json } = await placa_video.getGPU();
     
-    ws.getPriceValue(json, "placa_video");
+    await ws.getPriceValue(json, "placa_video");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
@@ -33,7 +33,7 @@ app.get('/processador', async (req, res) => {
     const processador = require("./../../resources/getjsons/processador");
     var { json } = await processador.getCPU();
     
-    ws.getPriceValue(json, "processador");
+    await ws.getPriceValue(json, "processador");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
@@ -42,7 +42,7 @@ app.get('/ram', async (req, res) => {
     const ram = require("./../../resources/getjsons/ram");
     var { json } = await ram.getRam();
     
-    ws.getPriceValue(json, "memram");
+    await ws.getPriceValue(json, "memram");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
@@ -50,8 +50,8 @@ app.get('/ram', async (req, res) => {
 app.get('/fonte', async (req, res) => {
     const fonte = require("./../../resources/getjsons/fonte");
     var { json } = await fonte.getFonte();
-    ws.getPriceValue(json, "fonte");
     
+    await ws.getPriceValue(json, "fonte");
     res.send("<marquee><h1> There's nothing around here! </h1></marquee>");
     
 });
